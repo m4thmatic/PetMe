2680835f6774537d1733e542f16a1330e5c806dd
mathmatic <digitaliox@gmail.com>
2024-06-16 20:27:44 -0400

    v2.0.4. Commented out debug line.

--- petGeneric.lua
+++ petGeneric.lua
@@ -39,7 +39,7 @@ gPet.statBars = function(pet)
     imgui.ProgressBar(pettp / 3000,  { windowSize/3-10, 15*gConfig.params.settings.window.scale[1] }, tostring(pettp));
     imgui.PopStyleColor(1);
 
-    imgui.Text("Status: " .. pet.Status);
+    --imgui.Text("Status: " .. pet.Status);
 
 end
 
